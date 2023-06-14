0c63e23727136a33c515762fae2d3ac162e0a6e3
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2023-06-14 15:28:08 +0700

    last commit

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -22,33 +22,92 @@ const index = async (req, res) => {
 	}
 }
 
-const store = (req, res) => {
-	// console.log(req.body)
-	res.json({
-		data: req.body,
-		message: 'POST users success'
-	})
+
+/**
+ * Store new user data to storage
+ * 
+ * @param req Request
+ * @param res Response
+ * @return mixed<array,json>
+ */
+const store = async (req, res) => {
+	const {body} = req
+
+	if (!body.name || !body.email) {
+		return res.status(400).json({
+			message: 'Bad Request',
+			data: null
+		})
+	}
+
+	try {
+		await userModel.createNewUser(body)
+
+		res.status(201).json({
+			message: 'CREATE new users success',
+			data: body
+		})
+	} catch (error) {
+		res.status(500).json({
+			message: 'Internal Server Error!',
+			serverMessage: error
+		})
+	}
 }
 
-const update = (req, res) => {
-	console.log(req.params)
-	res.json({
-		message: 'UPDATE user success',
-		data: req.body
-	})
+
+/**
+ * Update specific user data
+ * 
+ * @param req Request
+ * @param res Response
+ * @return mixed<array,json>
+ */
+const update = async (req, res) => {
+	const {body, params} = req
+
+	try {
+		await userModel.updateUser(body, params.id)
+
+		res.json({
+			message: 'UPDATE user success',
+			data: {
+				id: params.id, 
+				...body
+			}
+		})
+	} catch (error) {
+		res.status(500).json({
+			message: 'Internal Server Error!',
+			serverMessage: error
+		})
+	}
 }
 
-const destroy = (req, res) => {
+
+/**
+ * Delete user data from storage
+ * 
+ * @param req Request
+ * @param res Response
+ * @return mixed<array,json>
+ */ 
+const destroy = async (req, res) => {
 	const {id} = req.params
 
-	res.json({
-		message: 'DELETE user success',
-		data: {
-			id: id,
-			name: 'Rizqi Setiaji',
-			email: 'rizqisetiaji9@gmail.com'
-		}
-	})
+	try {
+		await userModel.deleteUser(id)
+
+		res.status(204).json({
+			message: 'DELETE user success',
+			data: null
+		})
+	} catch (error) {
+		res.status(500).json({
+			message: 'Internal Server Error!',
+			serverMessage: error
+		})
+	}
 }
 
 module.exports = { index, store, update, destroy }
--- models/users.js
+++ models/users.js
@@ -1,14 +1,61 @@
 const DB = require('../config/database')
 
 const table = 'users'
+const primaryKey = 'id'
+
 
 /**
  * Get all users data
+ * 
  * @return mixed
  */
 const getAllUsers = () => {
-	const SQLQuery = `SELECT * FROM ${table}`
+	const SQLQuery = `SELECT * FROM ${table} 
+      ORDER BY ${primaryKey} DESC LIMIT 100`
+
 	return DB.execute(SQLQuery)
 }
 
-module.exports = { getAllUsers }
+
+/**
+ * Create new user data
+ * 
+ * @param json request
+ * @return mixed
+ */
+const createNewUser = (request) => {
+   const SQLQuery = `INSERT INTO ${table} (name, email, address) 
+      VALUES ('${request.name}', '${request.email}', '${!request.address ? null : request.address}')`
+
+   return DB.execute(SQLQuery)
+}
+
+
+/**
+ * Update specific user data
+ * 
+ * @param json request
+ * @param int id
+ * @return mixed
+ */
+const updateUser = (request, id) => {
+   const SQLQuery = `UPDATE ${table} SET 
+      name='${request.name}', email='${request.email}', address='${request.address}' 
+      WHERE id=${id}`
+
+   return DB.execute(SQLQuery)
+}
+
+
+/**
+ * Delete specific user data from storage
+ * @param int id
+ * @return mixed
+ */ 
+const deleteUser = (id) => {
+   const SQLQuery = `DELETE FROM ${table} WHERE id=${id}`
+
+   return DB.execute(SQLQuery)
+}
+
+module.exports = { getAllUsers, createNewUser, updateUser, deleteUser }
